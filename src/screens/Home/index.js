bdb9304d6dac067d7638c39450bc026c6b919edb
Diogenes Oliveira <diogensgreen@gmail.com>
2020-10-19 19:23:22 -0300

    add checkebox

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
-import { useTheme } from "react-native-paper";
-
+import { useTheme, Checkbox } from "react-native-paper";
 import { Layout } from "../../components/";
 
 import { Context } from "../../context/";
@@ -9,6 +8,7 @@ import { Context } from "../../context/";
 export const Home = () => {
   const { name, setName, isDark, setIsDark } = React.useContext(Context);
   const { dark, colors, mode } = useTheme();
+  const [checked, setChecked] = React.useState(false);
   return (
     <Layout>
       <Text style={{ color: colors.primary }}>{name}</Text>
@@ -16,6 +16,19 @@ export const Home = () => {
         title="Btn"
         onPress={(e) => {
           console.log("oi", name);
+          setIsDark(!isDark);
+          if (name === "Desligado") {
+            setName("Ligado");
+          }
+          if (name === "Ligado") {
+            setName("Desligado");
+          }
+        }}
+      />
+      <Checkbox
+        status={checked ? "checked" : "unchecked"}
+        onPress={() => {
+          setChecked(!checked);
           setIsDark(!isDark);
           if (name === "Desligado") {
             setName("Ligado");
