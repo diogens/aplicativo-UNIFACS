69bbe91fe380a16b84cfdb3d83960dc0eda4b498
GuilhermeM314 <guimontenegro@hotmail.com.br>
2020-10-31 15:36:18 -0300

    Começando a API de heróis

--- src/screens/Herois/index.js
+++ src/screens/Herois/index.js
@@ -1,13 +1,32 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
-import { Layout } from "../../components/";
+import { View, Text, Button, FlatList } from "react-native";
 
-import { Context } from "../../context/";
+import { apiHero } from "../../services";
+import { Layout, Card as Box, UserList } from "../../components";
 
 export const Herois = () => {
+  const [hero, setHero] = React.useState([]);
+
+  React.useEffect(() => {
+    async function getHero() {
+
+      const res = await apiHero.get("/70");
+      setHero(res.data);
+    }
+    getHero();
+  }, [])
+
   return (
-    <Layout>
-      <Text>Seu projeto - Herois</Text>
+    <Layout title="Lista de Super Heróis">
+      <FlatList
+        data={hero}
+        renderItem={(item) => {
+          console.log(item)
+          return (
+            <Text>123</Text>
+          );
+        }}
+      />
     </Layout>
   );
-};
+}
--- src/services/index.js
+++ src/services/index.js
@@ -3,3 +3,9 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: `https://api.github.com`,
 });
+
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+
+export const apiHero = axios.create({
+  baseURL: `https://www.superheroapi.com/api.php/4645706468834592`,
+});
