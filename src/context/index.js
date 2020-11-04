fa23b29ed1e30e35a09576c36f0c1a7213a5d990
Diogenes Oliveira <diogensgreen@gmail.com>
2020-11-03 22:21:29 -0300

    context att

--- src/context/index.js
+++ src/context/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Context = React.createContext();
 
 export const AppContext = ({ children }) => {
-  const [isDark, setIsDark] = React.useState(false);
+  const [isDark, setIsDark] = React.useState(true);
   const [name, setName] = React.useState("desligado");
   return (
     <Context.Provider value={{ name, setName, isDark, setIsDark }}>
